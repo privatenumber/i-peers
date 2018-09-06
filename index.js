8eb2906fd1da784e055ae2a3d281c35d257f4f05
Hiroki Osame <hiroki.osame@gmail.com>
2018-09-06 16:45:41 -0400

    ready

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ var installList = Object.keys(peerDependencies).map(function(key) {
     return `${key}@${peerDependencies[key]}`
 });
 
-console.log('Installing', installList);
 
-// Lighter than installing npm as a dependency
-var npm = spawn('npm', ['install', '--no-save'].concat(installList));
-npm.stdout.pipe(process.stdout);
+if (installList.length) {
+	console.log('Installing', installList);
 
+	// Lighter than installing npm as a dependency
+	var npm = spawn('npm', ['install', '--no-save'].concat(installList));
+	npm.stdout.pipe(process.stdout);
+	npm.stderr.pipe(process.stderr);
+}
