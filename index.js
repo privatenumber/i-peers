4593264df11e01b0fcaca4adeb1d2903ca006958
Hiroki Osame <hiroki.osame@gmail.com>
2018-09-06 16:33:45 -0400

    Added log

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ var installList = Object.keys(peerDependencies).map(function(key) {
     return `${key}@${peerDependencies[key]}`
 });
 
+console.log('Installing', installList);
+
 // Lighter than installing npm as a dependency
 execSync('npm install --no-save ' + installList.join(' '));
