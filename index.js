758ab4c2505a5c28b2b4bd483bc523147ae24827
Hiroki Osame <hiroki.osame@gmail.com>
2018-09-06 16:29:16 -0400

    test

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 
 var path = require('path');
 var package = require(path.resolve('./package.json'));
+var npm = require('npm');
 
-console.log(package);
+var peerDependencies = package.peerDependencies || {};
+var installList = Object.keys(peerDependencies).map(function(key) {
+    return `${key}@${peerDependencies[key]}`
+});
+
+npm.load({ save: false }, function() {
+	npm.commands.install(installList);
+});
